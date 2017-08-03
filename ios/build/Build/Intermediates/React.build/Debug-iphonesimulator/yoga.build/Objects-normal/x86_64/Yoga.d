56721afe05446f3b2b352d32b9e603d57a6bbcca
Олегыч <olegyc@MacBook-Air-Olegyc.local>
2017-08-03 15:02:38 +0300

    server upload

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
@@ -1,6 +1,2 @@
 dependencies: \
-  /Users/olegyc/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c \
-  /Users/olegyc/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/olegyc/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/olegyc/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/olegyc/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/olegyc/Documents/Internship/DriveApp/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
